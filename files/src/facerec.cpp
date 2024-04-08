c4dc3a11953fa969246375abe24769319c118448
Conan-Doherty <conandoherty@rocketmail.com>
2024-04-08 13:09:00 +0100

    we

--- files/src/facerec.cpp
+++ files/src/facerec.cpp
@@ -2,18 +2,41 @@
 #include "opencv2/face.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
+#include "opencv2/core/utils/logger.hpp" // utils::logging::LOG_LEVEL_WARNING
 #include <iostream>
 #include <fstream>
 #include <filesystem>
-
-// g++ -std=c++17 facerec.cpp -lopencv_face -lopencv_core -lopencv_imgcodecs
+using namespace cv;
+using namespace std;
+//g++ -std=c++17 facerec.cpp -lopencv_face -lopencv_core -lopencv_imgcodecs
 
 int main(int argc, char *argv[])
 {
   namespace fs = std::filesystem;
+  
+  cv::Mat frame;
+  double fps = 30;
+  const char win_name[] = "Live Video...";
+
+  cv::utils::logging::setLogLevel(cv::utils::logging::LOG_LEVEL_SILENT);
+
+  std::cout << "Wait 60 secs. for camera access to be obtained..." << std::endl;
+  cv::VideoCapture vid_in(0);   // argument is the camera id
+
+  if (vid_in.isOpened())
+  {
+      std::cout << "Camera capture obtained." << std::endl;
+  }
+  else
+  {
+      std::cerr << "error: Camera 0 could not be opened for capture.\n";
+      return -1;
+  }
 
   std::vector<cv::Mat> images;
   std::vector<int>     labels;
+
+  
 
   // Iterate through all subdirectories, looking for .pgm files
   fs::path p(argc > 1 ? argv[1] : "../../att_faces");
@@ -27,7 +50,7 @@ int main(int argc, char *argv[])
       }
     }
   }
-
+  
   // Randomly choose an image, and remove it from the main collection
   std::srand(std::time(0));
   int rand_image_id = std::rand() % images.size();
@@ -37,11 +60,26 @@ int main(int argc, char *argv[])
   labels.erase(labels.begin() + rand_image_id);
   std::cout << "Actual class    = " << testLabel << '\n';
   std::cout << " training...";
-
+  
   cv::Ptr<cv::face::BasicFaceRecognizer> model = cv::face::EigenFaceRecognizer::create();
   model->train(images, labels);
   int predictedLabel = model->predict(testSample);
   std::cout << "\nPredicted class = " << predictedLabel << '\n';
+  
+  
+  cv::Point p1(260, 180);
+  cv::Point p2(260+91, 180+112);
+  cv::namedWindow(win_name);
+  while (1) {
+      
+      
+      vid_in >> frame;
+      rectangle(frame, p1, p2, Scalar(0, 255, 0));
+      //cv::flip(frame, flippedimage,1);
+      imshow(win_name, frame);
+      if (cv::waitKey(60 / fps) >= 0) // how long to wait for a key (milliseconds)
+          break;
+  }
 
   return 0;
 }
